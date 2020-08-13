c8dc28fee29ff1398660d053ac2f1ea4e42668c6
Jin <hanting.jin@theglue.com.au>
2020-08-13 11:02:18 +1000

    Finsih Signin and Signup function

--- src/components/signin/signin.js
+++ src/components/signin/signin.js
@@ -5,7 +5,7 @@ import FormInput from '../form-input/form-input';
 import CustomButton from '../custom-button/custom-button'
 
 // google sign-in
-import { signInWithGoogle } from '../../firebase/firebase.utils'
+import { auth, signInWithGoogle } from '../../firebase/firebase.utils'
 
 export default class Sign extends React.Component{
     constructor(){
@@ -17,10 +17,18 @@ export default class Sign extends React.Component{
         }
     }
 
-    handleSubmit = (event) => {
+    handleSubmit = async (event) => {
         event.preventDefault();
 
-        this.setState({email:'',password:''});
+        const {email, password} = this.state
+
+        try{
+            await auth.signInWithEmailAndPassword(email,password)
+            this.setState({email:'',password:''});
+        }catch(error){
+            console.log(error)
+        }
+      
     }
 
     handleChange = (event) => {
